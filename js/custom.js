eb9bab7857fa8ab7e70256815629d9466f3ca71e
amanyAbdelfattah <amanyabdelfattah58@gmail.com>
2021-12-15 17:32:36 +0200

    HNY

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,10 @@
-gsap.to('#santa', {
-    duration: 80,
-    y : -1000,
-    x : 2000
-}).repeat(Infinity)
+// gsap.to('#santa', {
+//     duration: 80,
+//     y : -1000,
+//     x : 2000
+// }).repeat(Infinity)
 
-gsap.from('#tree',{
-    duration: 30,
-    y : 8000,
-})
+// gsap.from('#tree',{
+//     duration: 30,
+//     y : 8000,
+// })
