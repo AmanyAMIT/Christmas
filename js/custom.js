752b924e749bce3d53a175352934415e48521945
amanyAbdelfattah <amanyabdelfattah58@gmail.com>
2021-12-15 14:06:56 +0200

    MarryChristmas

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,10 @@
 gsap.to('#santa', {
-
-    duration: 10,
+    duration: 80,
     y : -1000,
     x : 2000
 }).repeat(Infinity)
 
 gsap.from('#tree',{
-    duration: 6,
+    duration: 15,
     y : 8000,
 })
