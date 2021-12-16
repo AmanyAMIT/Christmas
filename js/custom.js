8ff1ca802430ae37088ad342c36d66b9d600e7b2
amanyAbdelfattah <amanyabdelfattah58@gmail.com>
2021-12-16 12:32:42 +0200

    New Year

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,30 @@
-gsap.to('#santa', {
-    duration: 80,
-    y : -1000,
-    x : 2000
-}).repeat(Infinity)
 
-gsap.from('#tree',{
-    duration: 30,
-    y : 8000,
-})
+
+function animation(NewYear){
+    if(NewYear.matches){
+        gsap.to('#santa', {
+            duration: 120,
+            y : -1000,
+            x : 2000
+        }).repeat(Infinity)
+        
+        gsap.from('#tree',{
+            duration: 10,
+            y : 8000,
+        })
+    } else{
+        gsap.to('#santa', {
+            duration: 80,
+            y : -1000,
+            x : 2000
+        }).repeat(Infinity)
+        
+        gsap.from('#tree',{
+            duration: 30,
+            y : 8000,
+        })
+    }
+}
+
+let NewYear = window.matchMedia("(max-width:280px)");
+animation(NewYear);
